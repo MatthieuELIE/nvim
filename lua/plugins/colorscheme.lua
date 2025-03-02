dcb1aa8fe450cf33972d00b0fc96cf6466b8027e
Matthieu ELIE <elie.matthieu2@gmail.com>
2025-03-02 11:26:43 +0100

    Add default options and keymaps

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,28 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = function()
-        require("catppuccin").load()
-      end,
-    },
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "mocha",
-      integrations = {
-        telescope = {
-          enabled = true,
-          style = "nvchad",
-        },
-        dropbar = {
-          enabled = true,
-          color_mode = true,
-        },
-      },
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = function()
+				require("catppuccin").load()
+			end,
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha",
+			integrations = {
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+				dropbar = {
+					enabled = true,
+					color_mode = true,
+				},
+			},
+		},
+	},
 }
